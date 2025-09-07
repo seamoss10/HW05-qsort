9097e0504fc35ddcb1888cb5fd558d1ce80cbce4
Daniel Soe <thetnsoe1@gmail.com>
2025-09-06 23:00:32 -0400

    HW5 done

--- student.c
+++ student.c
@@ -29,113 +29,100 @@ int countNewLine(FILE *fptr)
 }
 
 /* This is the first function you need to implement */
-bool StudentRead(char *filename, Student **stu, int *numelem)
-{
-    /* 1.1: open the file to read */
-    FILE *fptr=NULL;
-    // the name of the file to open is stored in filename
-    // if fopen fails, return false
-    // do not use fclose since fopen already fails
+bool StudentRead(char *filename, Student **stu, int *numelem) {
+    FILE *fp = NULL;
+    fp = fopen(filename, "r");
 
-    /* end of 1.1: open the file to read */
-
-    int numline = countNewLine(fptr);
-
-    // return to the beginning of the file
-    // you can use fseek or
-    // fclose followed by fopen
-    // You need to check whether fseek or fopen fails
-    // Do not use rewind because it does not report whether it fails
-    int rtv; // return value
-    rtv = fseek(fptr, 0, SEEK_SET);
-    if (rtv == -1) // fseek fails
-    {
-        fclose(fptr);
+    if(!fp) {
         return false;
     }
 
-    /* 1.2 allocate memory for the data */
+    int numline = countNewLine(fp);
+
+    int rtv;
+    rtv = fseek(fp, 0, SEEK_SET);
+    if (rtv == -1)
+    {
+        fclose(fp);
+        return false;
+    }
+
     Student *stuptr = NULL;
-    // stuptr is an array of type Student
-    // refer to hw5.h to understand the type Student
-    // the number of elements in this array is numline
-    // use the malloc function
-    // check whether memory allocation fails
+    stuptr = (Student *) malloc(numline * sizeof(Student)); // Pointer to an array of students
 
-    /* end of 1.2: allocate memory for the data */
-
-    /* 1.3: read data from the file */
-    // read the data from the file
-    // store the data to the array stuptr
-    // fclose the file after read of data is done
-
-    /* end of 1.3: allocate memory for the data */
+    for (int i = 0; i < numline; i++) {
+        int ret = fscanf(fp, "%d %s", &stuptr[i].ID, stuptr[i].name);
+        if (ret != 2) {
+            fclose(fp);
+            return false;
+        }
+    }
 
     *numelem = numline;
     *stu = stuptr;
+    fclose(fp);
     return true;
 }
 
 /* This is the second function you need to implement */
-bool StudentWrite(char *filename, Student *stu, int numelem)
-{
-    // open the file to write
-    // the name of file to open is stored in string filename
-    // if fopen fails, return false
-    // do not use fclose since fopen already fails
-    // stu is an array of type Student
-    // refer to hw5.h to understand the type Student
-    // the number of elements in array stu is numelem
-    // write the data from array stu to the opened file
-    // fclose the file in the end
+bool StudentWrite(char *filename, Student *stu, int numelem) {
+    FILE *fp = NULL;
+    fp = fopen(filename, "w");
 
+    if(!fp) {
+        return false;
+    }
+
+    for (int i = 0; i < numelem; i++) {
+        int res = fprintf(fp, "%d %s\n", stu[i].ID, stu[i].name);
+        if (res < 0) {
+            fclose(fp);
+            return false;
+        }
+    }
+
+    fclose(fp);
     return true;
 }
 
 /* This is the third function you need to implement */
-void sortStudents(Student *stu, int numelem, int (*compar)(const void *, const void *))
-{
-    /* Fill in to call qsort function to sort array stu */
-    // stu: an array of Students. numelem: number of elements in the array. compar: comparison function
-    // refer to hw5.h to understand the type Student
+void sortStudents(Student *stu, int numelem, int (*compar)(const void *, const void *)) {
+    qsort(stu, numelem, sizeof(Student), compar);
 }
 
 /* This is the fourth function you need to implement */
-int compareID(const void *p1, const void *p2)
-{
-    /* Fill in to compare p1 and p2 by id */
-    // return a negative value if the ID of the first student is smaller
-    // return a positive value if the ID of the first student is larger
-    // return zero if the IDs of the two students are the same
+int compareID(const void *p1, const void *p2) {
+    Student *stu1 = (Student *) p1;
+    Student *stu2 = (Student *) p2;
 
-    // dummy return to avoid compile error
-    // you need to replace it with your own return statement
+    if (stu1->ID > stu2->ID)  return 1;
+    if (stu1->ID < stu2->ID)  return -1;
     return 0;
 }
 
 /* This is the fifth function you need to implement */
-int compareName(const void *p1, const void *p2)
-{
-    /* Fill in to compare p1 and p2 by name */
-    // use strcmp function to compare two strings
-    // return a negative value if the name of the first student is alphabetically earlier
-    // return a positive value if the name of the first student is alphabetically later
-    // return zero if the names of the two students are the same
+int compareName(const void *p1, const void *p2) {
+    Student *stu1 = (Student *) p1;
+    Student *stu2 = (Student *) p2;
 
-    // dummy return to avoid compile error
-    // you need to replace it with your own return statement
+    int res = strcmp(stu1->name, stu2->name);
+
+    if (res != 0) {
+        return res;
+    }
+
+    if (stu1->ID > stu2->ID)  return 1;
+    if (stu1->ID < stu2->ID)  return -1;
     return 0;
 }
 
 /* This is the sixth function you need to implement */
-bool areStudentsSorted(Student *stu, int numelem, int (*compar)(const void *, const void *))
-{
-    /* Fill in to check if the stu array is sorted according to compar */
-    // return true if the stu array is sorted according to compar
-    // return false otherwise
-    // refer to hw5.h to understand the type Student
-
-    // dummy return to avoid compile error
-    // you need to replace it with your own return statement
+bool areStudentsSorted(Student *stu, int numelem, int (*compar)(const void *, const void *)) {
+    for (int i = 0; i < numelem - 1; i++) {
+        int res = compar(&stu[i + 1], &stu[i]);
+        if (res < 0) {
+            return false;
+        }
+    }
     return true;
 }
